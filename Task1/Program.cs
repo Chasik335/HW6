b822c9a433327dc29b3f777008aafbef11f0da1e
Vladislav <vhasovnikov@inbox.ru>
2023-01-14 23:39:05 +0500

    Написал код, сделал кнопки

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] CreateFillArray()
+{
+    Console.Write("Введите четное число: ");
+    int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+void CountPositiveNumber(int[] colection)
+{
+    int count = 0;
+    for (int i = 0; i < colection.Length; i++)
+    {
+        if (colection[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("Количество чисел которые больше 0 -> " + count);
+}
+int[] arr = CreateFillArray();
+CountPositiveNumber(arr);
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿double GetValue(string text)
+{
+  Console.Write(text);
+  return Convert.ToDouble(Console.ReadLine());
+}
+void DotIntersections(double b1, double b2, double k1, double k2)
+{
+    double y = k1 * (b2 - b1)/(k1 - k2) + b1;
+    double x = (b2 - b1)/(k1 - k2);
+Console.WriteLine($"Точка пересечения двух прямых -> ({Math.Round(x, 2)};{Math.Round(y, 2)})");
+}
+double b1 = GetValue("Введите b1: ");
+double b2 = GetValue("Введите b2: ");
+double k1 = GetValue("Введите k1: ");
+double k2 = GetValue("Введите k2: ");
+DotIntersections(b1, b2, k1, k2);
