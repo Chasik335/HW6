ab02fb019242e160c0db52d44a8866464007d59a
Vladislav <vhasovnikov@inbox.ru>
2023-01-15 15:16:12 +0500

    Внес изменения в код файл Task2/Program

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,9 +5,13 @@
 }
 void DotIntersections(double b1, double b2, double k1, double k2)
 {
-    double y = k1 * (b2 - b1)/(k1 - k2) + b1;
-    double x = (b2 - b1)/(k1 - k2);
-Console.WriteLine($"Точка пересечения двух прямых -> ({Math.Round(x, 2)};{Math.Round(y, 2)})");
+    if (b1 != k1 & b2 != k2)
+    {
+        double y = k1 * (b2 - b1)/(k1 - k2) + b1;
+        double x = (b2 - b1)/(k1 - k2);
+        Console.WriteLine($"Точка пересечения двух прямых -> ({Math.Round(x, 2)};{Math.Round(y, 2)})");
+    }
+    else Console.WriteLine("Прямые не пересекаются");
 }
 double b1 = GetValue("Введите b1: ");
 double b2 = GetValue("Введите b2: ");
